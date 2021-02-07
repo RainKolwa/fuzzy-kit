00c72ff9acebdc544886205a0dde2ad5884f1e80
rainkolwa <rainkolwa@gmail.com>
2021-02-07 16:08:30 +0800

    feat: smui

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
 	export let name;
+	import Button from '@smui/button';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Button on:click={() => alert('Clicked!')}>Just a Button</Button>
 </main>
 
 <style>
